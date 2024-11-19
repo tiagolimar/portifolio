29262a3a4e7fa01ab58e060ba11c31d16a09a30d
tiagolimar <tiagolima.r2@gmail.com>
2024-11-19 15:26:21 -0300

    restart project

---
+++ js/script.js
@@ -0,0 +1,101 @@
+const experiencia1 = {
+    cargo: 'Professor Frontend Javascript',
+    empresa: 'Digital College Brasil',
+    cidade: 'Fortaleza, CE',
+    periodo: 'Junho 2024 - Atual',
+    atividade: [
+        `Ministrei aulas de reposição em Desenvolvimento Web Fullstack, abrangendo HTML, CSS, JavaScript, Git e React.`,
+        `Auxiliei alunos no entendimento de conceitos fundamentais de Frontend e na prática de desenvolvimento colaborativo com Git.`,
+        `Demonstrei técnicas de debugging e boas práticas de desenvolvimento para interfaces dinâmicas e responsivas.`
+    ],
+}
+
+const experiencia2 = {
+    cargo: 'Projetista/Técnico em Edificações',
+    empresa: 'FD Consultoria e Projetos LTDA',
+    cidade: 'Fortaleza, CE',
+    periodo: 'Fevereiro 2014 - Atual',
+    atividade: [
+        `Desenvolvi projetos hidrossanitários, estruturais e arquitetônicos em AutoCAD, Revit e SketchUp, automatizando tarefas com scripts em Python.`,
+        `Criei aplicações web para cálculos e dimensionamentos, solucionando problemas práticos na engenharia.`,
+        `Renderizei modelos 3D e produzi materiais visuais para apresentações de projetos, gerindo prazos e colaborando com equipes multidisciplinares.`
+    ],
+}
+
+const experiencia3 = {
+    cargo: 'Bolsista',
+    empresa: 'Instituto Federal de Educação, Ciência e Tecnologia do Ceará (IFCE)',
+    cidade: 'Fortaleza, CE',
+    periodo: 'Setembro 2012 - Janeiro 2014',
+    atividade: [
+        `Colaborei em projetos de reforma com AutoCAD e SketchUp, focando na precisão técnica.`,
+        `Acompanhei serviços de manutenção predial, participando de reuniões e aprimorando habilidades de comunicação e resolução de problemas.`
+    ],
+}
+
+const portifolio = {
+    dados_pessoais: {
+        nome: 'Tiago Lima',
+        profissao: 'Desenvolvedor Web',
+        cidade: 'Fortaleza, CE',
+    },
+    sessao_resumo: {
+        titulo: 'Resumo Profissional',
+        descricao: 'Profissional com 29 anos de experiência combinada em engenharia civil e tecnologia, especializado em desenvolvimento web, automação de projetos e modelagem 3D. Com uma década em projetos de instalações prediais, modelagem 3D e utilizando scripts Python, tenho experiência prática como professor e desenvolvedor Fullstack. Estou em transição de carreira para a área de tecnologia, onde aplico minhas habilidades em programação para otimizar processos, resolver problemas reais e agregar valor em projetos desafiadores.',
+    },
+    sessao_experiencia: {
+        titulo: 'Experiência Profissional',
+        itens: [experiencia1, experiencia2, experiencia3]
+    }
+}
+
+
+`
+________________________________________
+Formação Acadêmica
+Tecnólogo em Análise e Desenvolvimento de Sistemas (em andamento)
+Instituto Federal do Ceará - IFCE | Fevereiro 2023 - Julho 2025
+•	Foco em desenvolvimento de software, banco de dados, padrões de arquitetura e estruturas de dados.
+•	Principais competências: JavaScript, Python, PHP, SQL, API REST, controle de versão com Git, padrões de design e ciência da computação.
+Curso de Desenvolvimento Full-Stack
+Digital College Brasil | Dezembro 2022 - Janeiro 2024
+•	Formação prática em front-end e back-end com Node.js, React e bancos de dados relacionais.
+•	Competências desenvolvidas: Princípios SOLID, modelagem de banco de dados, programação lógica e Git.
+Curso Técnico em Edificações
+Instituto Federal do Ceará - IFCE | Janeiro 2011 - Julho 2015
+•	Especialização em construção de edifícios com desenho técnico, modelagem 3D e resolução de problemas em projetos arquitetônicos.
+Qualificação Tecnológica em Informática
+Brasil.com | Outubro 2011 - Novembro 2013
+•	Competências em HTML, CorelDRAW, Photoshop, Dreamweaver, hardware e redes.
+Formação Avançada em Back-End com PHP e Laravel (em andamento)
+Digital College Brasil | Julho 2024 - Janeiro 2025
+•	Curso intensivo de PHP e Laravel com foco em desenvolvimento web escalável e boas práticas de arquitetura.
+
+________________________________________
+Habilidades Técnicas
+•	Desenvolvimento Web: JavaScript, React, Node.js, Express, PHP, Laravel, HTML, CSS, Bootstrap, Git & GitHub, SQL.
+•	Automação e Scripts: Python, C# para plugins em Revit.
+•	Modelagem e Design 3D: AutoCAD, Revit, SketchUp, V-Ray.
+•	Ferramentas de Edição: Adobe Photoshop, CorelDRAW.
+•	Gerenciamento e Organização: Desenvolvimento e automação de projetos técnicos, coordenação de equipes, documentação técnica.
+________________________________________
+Projetos Relevantes
+FDProjetos - Plugin de Automação para Revit
+Desenvolvi um plugin em Python,  C# e Dynamo para automação de tarefas no Autodesk Revit, otimizando processos em projetos hidrossanitários e reduzindo o tempo de execução de tarefas.
+DC Store App – Plataforma de Ecommerce Full-Stack
+Desenvolvi o DC Store App, uma aplicação de e-commerce com propósito educacional, demonstrando a criação de uma plataforma web moderna e responsiva. O projeto inclui:
+•	Front-End em React: Interface de usuário dinâmica com navegação eficiente e design responsivo usando React, React Router e Bootstrap.
+•	Back-End em Node.js e Express: Estrutura de API REST, manipulando requisições e persistindo dados com PostgreSQL e Sequelize.
+•	Deploy em Vercel e Render: O front-end está hospedado na Vercel e o back-end na Render, garantindo uma experiência de usuário fluida e acessível.
+•	Funcionalidades de Ecommerce: Inclui páginas de produtos, categorias, pedidos e navegação organizada, proporcionando uma experiência completa para o usuário.
+Eternity – Site para Comunidade de Minecraft
+Desenvolvi o site para o Clã Eternity, uma comunidade de jogadores de Minecraft do servidor Armageddon, com foco em funcionalidades úteis para membros e visitantes:
+•	Página Principal: Informações sobre o clã e navegação para várias seções e formulário de recrutamento de novos membros.
+•	Painel Administrativo: Seção restrita para gestão de membros, com tabelas interativas e opções de busca e filtro, além de ações de gerenciamento, como banir e ativar membros.
+•	Tecnologias Utilizadas: HTML5, CSS3, JavaScript, LightGallery, Fetch API para requisições e Cookie API para autenticação segura.
+________________________________________
+Competências Comportamentais
+•	Comunicação e Didática: Experiência com ensino e suporte técnico, explicando conceitos complexos de forma acessível.
+•	Liderança e Trabalho em Equipe: Coordenação de equipes e experiência em desenvolvimento colaborativo.
+•	Organização e Proatividade: Planejamento de projetos e automação de processos com foco em eficiência.
+`
